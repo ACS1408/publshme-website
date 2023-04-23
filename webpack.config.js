386f02b7755a6374cde0bbd43ee0f81090ab2127
Abhilash C S <abhilash.acs14@gmail.com>
2023-04-23 15:18:13 +0530

    structure updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "assets/js/main.js"),
+  entry: path.resolve(__dirname, "public/assets/js/main.js"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
   },
   module: {
